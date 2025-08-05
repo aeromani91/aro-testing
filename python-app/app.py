4a4b8a8e0dfd6aff1e432b10cfa294719030a933
Kamaraj <manikandan.kamaraj@accenture.com>
2025-08-05 11:31:03 +0530

    python app updated

--- python-app/app.py
+++ python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Docker! Update 1"
+    return "Hello from Docker! Update 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
